390f7f0a378e1630cd780c3ac03ff03cce5aa3a7
taidungit <dungmount75@gmail.com>
2026-06-07 01:09:38 +0700

    fix code

--- src/main/java/com/singeasy/booking_service/dto/req/UserReqDto.java
+++ src/main/java/com/singeasy/booking_service/dto/req/UserReqDto.java
@@ -1,11 +1,17 @@
 package com.singeasy.booking_service.dto.req;
 
 import com.singeasy.booking_service.enums.RoleEnum;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UserReqDto {
+    @NotBlank(message = "Name is required")
     private String name;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
     private String password;
     private String avatar;
